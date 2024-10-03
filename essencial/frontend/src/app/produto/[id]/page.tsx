cb05f77c83dd696013d6c5f486a37d0b688af8da
Bruno H. Homem <brunohhomem@gmail.com>
2024-10-03 19:26:22 -0300

    chore: added template to pages

--- essencial/frontend/src/app/produto/[id]/page.tsx
+++ essencial/frontend/src/app/produto/[id]/page.tsx
@@ -1,7 +1,9 @@
-import { produtos } from '@/core'
+import useProdutos from '@/data/hooks/useProdutos'
 
 export default function PaginaProduto(props: any) {
   const id = +props.params.id
+  const { produtos } = useProdutos()
   const produto = produtos.find(p => p.id === id)
+
   return produto ? <div>{produto.nome}</div> : <div>Produto não encontrado</div>
 }

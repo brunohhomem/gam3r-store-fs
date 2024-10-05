71df20d5fa27ae81abcdbd877d5fa2a03aa06c03
Bruno H. Homem <brunohhomem@gmail.com>
2024-10-05 15:22:18 -0300

    feat: added CRUD methods for produtos

---
+++ essencial/backend/src/produto/produto.prisma.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaProvider } from './../db/prisma.provider';
+import { Produto } from '../core';
+
+@Injectable()
+export class ProdutoPrisma {
+  constructor(readonly prisma: PrismaProvider) {}
+
+  async salvar(produto: Produto): Promise<void> {
+    await this.prisma.produto.upsert({
+      where: { id: produto.id ?? -1 },
+      update: produto,
+      create: produto,
+    });
+  }
+
+  async obter(): Promise<Produto[]> {
+    return this.prisma.produto.findMany() as any;
+  }
+
+  async obterPorId(id: number): Promise<Produto | null> {
+    const produto = await this.prisma.produto.findUnique({ where: { id } });
+    return produto as any;
+  }
+
+  async excluir(id: number): Promise<void> {
+    await this.prisma.produto.delete({ where: { id } });
+  }
+}

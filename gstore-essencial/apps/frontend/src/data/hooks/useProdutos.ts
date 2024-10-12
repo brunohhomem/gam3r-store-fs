05328243b18df4cf102b92059f2b3e299672ff93
Bruno H. Homem <brunohhomem@gmail.com>
2024-10-12 15:28:22 -0300

    chore: added and updated custom hooks

--- gstore-essencial/apps/frontend/src/data/hooks/useProdutos.ts
+++ gstore-essencial/apps/frontend/src/data/hooks/useProdutos.ts
@@ -5,28 +5,33 @@ import { useCallback, useEffect, useState } from 'react'
 const urlBase = 'http://localhost:4000'
 
 export default function useProdutos() {
-  const [produtos, setProdutos] = useState<Produto[]>([])
+    const [produtos, setProdutos] = useState<Produto[]>([])
 
-  async function obterProdutos(): Promise<Produto[]> {
-    const resp = await fetch(`${urlBase}/produtos`)
-    const produtos = await resp.json()
-    return produtos ?? []
-  }
+    async function obterProdutos(): Promise<Produto[]> {
+        const resp = await fetch(`${urlBase}/produtos`)
+        const produtos = await resp.json()
+        return produtos ?? []
+    }
 
-  const obterProdutoPorId = useCallback(async function obterProdutoPorId(
-    id: number
-  ): Promise<Produto | null> {
-    const resp = await fetch(`${urlBase}/produtos/${id}`)
-    const produto = await resp.json()
-    return produto ?? null
-  }, [])
+    const obterProdutoPorId = useCallback(async function obterProdutoPorId(
+        id: number
+    ): Promise<Produto | null> {
+        try {
+            const resp = await fetch(`${urlBase}/produtos/${id}`)
+            const produto = await resp.json()
+            return produto ?? null
+        } catch (error) {
+            console.error('Erro ao obter produto por id', error)
+            return null
+        }
+    }, [])
 
-  useEffect(() => {
-    obterProdutos().then(setProdutos)
-  }, [])
+    useEffect(() => {
+        obterProdutos().then(setProdutos)
+    }, [])
 
-  return {
-    produtos,
-    obterProdutoPorId
-  }
+    return {
+        produtos,
+        obterProdutoPorId,
+    }
 }

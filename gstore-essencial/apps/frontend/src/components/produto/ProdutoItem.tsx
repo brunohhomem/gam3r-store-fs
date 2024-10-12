bcddb401a7ab9b614f2d5d918ac03148308e7813
Bruno H. Homem <brunohhomem@gmail.com>
2024-10-12 15:29:01 -0300

    chore: added new produto components

--- gstore-essencial/apps/frontend/src/components/produto/ProdutoItem.tsx
+++ gstore-essencial/apps/frontend/src/components/produto/ProdutoItem.tsx
@@ -4,6 +4,7 @@ import { Produto } from '@gstore/core'
 import { IconShoppingCartPlus } from '@tabler/icons-react'
 import Image from 'next/image'
 import Link from 'next/link'
+import NotaReview from '../shared/NotaReview'
 
 export interface ProdutoItemProps {
   produto: Produto
@@ -17,6 +18,11 @@ export default function ProdutoItem(props: ProdutoItemProps) {
       href={`/produto/${produto.id}`}
       className="flex flex-col bg-violet-dark border border-white/10 rounded-xl relative max-w-[350px]"
     >
+
+      <div className='absolute flex justify-end top-2.5 right-2.5'>
+        <NotaReview nota={produto.nota}/>
+      </div>
+
       <div className="h-48 w-full relative">
         <Image
           src={produto.imagem}
